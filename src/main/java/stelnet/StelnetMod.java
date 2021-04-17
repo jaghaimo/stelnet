9dcca71fa32a3f9e0a3eae37b27691ffb060472c
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-04-17 09:59:57 +0100

    Add listener purging

--- src/main/java/stelnet/StelnetMod.java
+++ src/main/java/stelnet/StelnetMod.java
@@ -1,6 +1,8 @@
 package stelnet;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.campaign.listeners.ListenerManagerAPI;
+import com.fs.starfarer.api.campaign.listeners.ListenerUtil;
 
 import stelnet.commodity.CommodityBoard;
 import stelnet.helper.GlobalHelper;
@@ -9,6 +11,7 @@ import stelnet.helper.LogHelper;
 import stelnet.market.MarketQueryBoard;
 import stelnet.market.MonthEndListener;
 import stelnet.storage.StorageBoard;
+import stelnet.storage.StorageListener;
 
 public class StelnetMod extends BaseModPlugin {
 
@@ -26,18 +29,39 @@ public class StelnetMod extends BaseModPlugin {
 
     private void init() {
         if (GlobalHelper.isDevMode()) {
-            purge(CommodityBoard.class, StorageBoard.class, MarketQueryBoard.class);
+            purgeIntel(CommodityBoard.class, StorageBoard.class, MarketQueryBoard.class);
+            purgeListeners(MonthEndListener.class, StorageListener.class);
         }
-        CommodityBoard.getInstance();
-        StorageBoard.getInstance();
-        MarketQueryBoard.getInstance();
-        MonthEndListener.register();
+        initCommodity();
+        initMarket();
+        initStorage();
         LogHelper.debug("Initiation complete");
     }
 
-    private void purge(Class<?>... classNames) {
+    private void initCommodity() {
+        CommodityBoard.getInstance();
+    }
+
+    private void initMarket() {
+        MarketQueryBoard.getInstance();
+        MonthEndListener.register();
+    }
+
+    private void initStorage() {
+        StorageBoard.getInstance();
+        StorageListener.register();
+    }
+
+    private void purgeIntel(Class<?>... classNames) {
         for (Class<?> className : classNames) {
             IntelHelper.purgeIntel(className);
+        }
+    }
+
+    private void purgeListeners(Class<?>... classNames) {
+        ListenerManagerAPI listenerManagerAPI = GlobalHelper.getListenerManager();
+        for (Class<?> className : classNames) {
+            listenerManagerAPI.removeListenerOfClass(className);
         }
     }
 }
