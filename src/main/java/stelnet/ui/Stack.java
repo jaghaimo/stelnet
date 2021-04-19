a197e622377f0e24fddb1963b734282935bfc8d9
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-04-19 12:06:38 +0100

    Add option to render Stack in reversed direction

--- src/main/java/stelnet/ui/Stack.java
+++ src/main/java/stelnet/ui/Stack.java
@@ -12,12 +12,27 @@ import com.fs.starfarer.api.ui.CustomPanelAPI;
  */
 public class Stack extends Group {
 
+    private int direction;
+
+    public Stack(boolean reversed, Renderable... elements) {
+        super(Arrays.asList(elements));
+        if (reversed) {
+            direction = -1;
+        }
+    }
+
     public Stack(Renderable... elements) {
         super(Arrays.asList(elements));
+        direction = 1;
     }
 
     public Stack(List<Renderable> elements) {
         super(elements);
+        direction = 1;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
     }
 
     @Override
@@ -25,7 +40,7 @@ public class Stack extends Group {
         for (Renderable renderable : getElements()) {
             Size size = renderable.getSize();
             renderable.render(panel, x, y);
-            y += size.getHeigth();
+            y += size.getHeigth() * direction;
         }
     }
 }
