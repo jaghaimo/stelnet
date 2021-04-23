772b57c1c6a8d5ec5aca3fe5d9418e4023468bfe
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-04-23 21:20:41 +0100

    Add shortcuts to control row

--- src/main/java/stelnet/market/view/ToggleAllButton.java
+++ src/main/java/stelnet/market/view/ToggleAllButton.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.fs.starfarer.api.ui.IntelUIAPI;
 import com.fs.starfarer.api.util.Misc;
 
+import org.lwjgl.input.Keyboard;
+
 import stelnet.market.IntelQuery;
 import stelnet.ui.Button;
 import stelnet.ui.SimpleCallback;
@@ -14,6 +16,7 @@ public class ToggleAllButton extends Button {
 
     public ToggleAllButton(final List<IntelQuery> queries) {
         super(new Size(120, 24), "Toggle All", !queries.isEmpty(), Misc.getButtonTextColor());
+        setShortcut(Keyboard.KEY_T);
         setCallback(new SimpleCallback() {
 
             @Override
--- src/main/java/stelnet/market/view/UpdateAllButton.java
+++ src/main/java/stelnet/market/view/UpdateAllButton.java
@@ -5,15 +5,18 @@ import java.util.List;
 import com.fs.starfarer.api.ui.IntelUIAPI;
 import com.fs.starfarer.api.util.Misc;
 
+import org.lwjgl.input.Keyboard;
+
 import stelnet.market.IntelQuery;
 import stelnet.ui.Button;
 import stelnet.ui.SimpleCallback;
 import stelnet.ui.Size;
 
-public class RefreshAllButton extends Button {
+public class UpdateAllButton extends Button {
 
-    public RefreshAllButton(final List<IntelQuery> queries) {
-        super(new Size(120, 24), "Refresh All", !queries.isEmpty(), Misc.getButtonTextColor());
+    public UpdateAllButton(final List<IntelQuery> queries) {
+        super(new Size(120, 24), "Update All", !queries.isEmpty(), Misc.getButtonTextColor());
+        setShortcut(Keyboard.KEY_U);
         setCallback(new SimpleCallback() {
             @Override
             public void confirm(IntelUIAPI ui) {
