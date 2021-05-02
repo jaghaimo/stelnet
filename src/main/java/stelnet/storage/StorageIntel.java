ab19628371dc1de4fe32e1fbcc996185eadcdbe8
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-05-02 15:39:42 +0100

    Refactor createSmallIntel

--- src/main/java/stelnet/storage/StorageIntel.java
+++ src/main/java/stelnet/storage/StorageIntel.java
@@ -1,17 +1,22 @@
 package stelnet.storage;
 
 import java.awt.Color;
+import java.util.Arrays;
 import java.util.List;
 
 import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.econ.SubmarketAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.ui.Alignment;
-import com.fs.starfarer.api.ui.TooltipMakerAPI;
 
 import stelnet.BaseIntel;
 import stelnet.IntelInfo;
 import stelnet.helper.CargoHelper;
+import stelnet.ui.Cargo;
+import stelnet.ui.Heading;
+import stelnet.ui.Renderable;
+import stelnet.ui.Ships;
+import stelnet.ui.Size;
+import stelnet.ui.Spacer;
 
 public class StorageIntel extends BaseIntel {
 
@@ -22,19 +27,6 @@ public class StorageIntel extends BaseIntel {
     public StorageIntel(SubmarketAPI storage) {
         super(storage.getMarket().getFaction(), storage.getMarket().getPrimaryEntity());
         this.storage = storage;
-    }
-
-    @Override
-    public void createSmallDescription(TooltipMakerAPI info, float width, float height) {
-        Color baseColor = getFactionForUIColors().getBaseUIColor();
-        Color darkColor = getFactionForUIColors().getDarkUIColor();
-        info.addSectionHeading(getLocationName() + " Items", baseColor, darkColor, Alignment.MID, 5f);
-        info.addSpacer(10);
-        showItems(info);
-        info.addSpacer(10);
-        info.addSectionHeading(getLocationName() + " Ships", baseColor, darkColor, Alignment.MID, 5f);
-        info.addSpacer(10);
-        showShips(info);
     }
 
     @Override
@@ -49,6 +41,22 @@ public class StorageIntel extends BaseIntel {
     }
 
     @Override
+    protected List<Renderable> getRenderables(Size size) {
+        Color baseColor = getFactionForUIColors().getBaseUIColor();
+        Color darkColor = getFactionForUIColors().getDarkUIColor();
+        CargoAPI cargo = storage.getCargo();
+        List<FleetMemberAPI> ships = storage.getCargo().getMothballedShips().getMembersListCopy();
+        return Arrays.<Renderable>asList(//
+                new Heading(getLocationName() + " Items", baseColor, darkColor), //
+                new Spacer(10), //
+                new Cargo(cargo, "There are no items in this storage.", size), //
+                new Spacer(10), //
+                new Heading(getLocationName() + " Ships", baseColor, darkColor), //
+                new Spacer(10), //
+                new Ships(ships, "There are no ships in this storage.", size));
+    }
+
+    @Override
     protected String getTag() {
         return TAG;
     }
@@ -60,16 +68,5 @@ public class StorageIntel extends BaseIntel {
         String items = itemsCount != 1 ? "s" : "";
         String ships = shipsCount != 1 ? "s" : "";
         return String.format("%d item%s & %d ship%s", itemsCount, items, shipsCount, ships);
-    }
-
-    private void showItems(TooltipMakerAPI info) {
-        CargoAPI cargo = storage.getCargo().createCopy();
-        cargo.sort();
-        info.showCargo(cargo, cargo.getStacksCopy().size(), false, 5f);
-    }
-
-    private void showShips(TooltipMakerAPI info) {
-        List<FleetMemberAPI> ships = storage.getCargo().getMothballedShips().getMembersListCopy();
-        info.showShips(ships, ships.size(), false, 5f);
     }
 }
