be917fd677c7f90c1120178c40f11ae58e6992fc
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-05-03 07:52:06 +0100

    Standarise small intel classes (#39)

--- src/main/java/stelnet/market/MarketResultIntel.java
+++ src/main/java/stelnet/market/MarketResultIntel.java
@@ -1,67 +1,32 @@
 package stelnet.market;
 
-import java.awt.Color;
-import java.util.Set;
-
 import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
-import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
-import com.fs.starfarer.api.ui.SectorMapAPI;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.api.util.Misc;
 
-import stelnet.helper.StarSystemHelper;
+import stelnet.BaseIntel;
+import stelnet.IntelInfo;
 
-public class MarketResultIntel extends BaseIntelPlugin {
+public class MarketResultIntel extends BaseIntel {
 
     public final static String TAG = "stelnetMarket";
 
-    private final FactionAPI faction;
-    private final SectorEntityToken sectorEntityToken;
     private final IntelSubject intelSubject;
 
     public MarketResultIntel(FactionAPI f, SectorEntityToken s, IntelSubject i) {
-        faction = f;
-        sectorEntityToken = s;
+        super(f, s);
         intelSubject = i;
     }
 
     @Override
-    public void createIntelInfo(TooltipMakerAPI info, ListInfoMode mode) {
-        Color bulletColor = getBulletColorForMode(mode);
-        info.addPara(intelSubject.getIntelTitle(), getTitleColor(mode), 0f);
-        info.beginGridFlipped(300f, 1, Misc.getTextColor(), 80f, 10f);
-        info.addToGrid(0, 0, sectorEntityToken.getName(), "Location", bulletColor);
-        info.addToGrid(0, 1, faction.getDisplayName(), "Faction", bulletColor);
-        info.addToGrid(0, 2, StarSystemHelper.getName(sectorEntityToken.getStarSystem()), "System", bulletColor);
-        info.addGrid(3f);
-    }
-
-    @Override
     public void createSmallDescription(TooltipMakerAPI info, float width, float height) {
+        // TODO: redo using Renderable
         intelSubject.createSmallDescription(info, width, height);
     }
 
     @Override
     public String getIcon() {
         return intelSubject.getIcon();
-    }
-
-    @Override
-    public SectorEntityToken getMapLocation(SectorMapAPI map) {
-        return sectorEntityToken;
-    }
-
-    @Override
-    public FactionAPI getFactionForUIColors() {
-        return faction;
-    }
-
-    @Override
-    public Set<String> getIntelTags(SectorMapAPI map) {
-        Set<String> tags = super.getIntelTags(map);
-        tags.add(TAG);
-        return tags;
     }
 
     @Override
@@ -74,13 +39,9 @@ public class MarketResultIntel extends BaseIntelPlugin {
     }
 
     @Override
-    public String getSortString() {
-        return getDistanceToPlayerLY("%07.2f");
-    }
-
-    @Override
-    public boolean isNew() {
-        return false;
+    protected IntelInfo getIntelInfo() {
+        return new IntelInfo(intelSubject.getIntelTitle(), "Location", getLocationNameWithSystem(), "Faction",
+                getFactionWithRel());
     }
 
     private void endIfInvalid() {
@@ -93,8 +54,8 @@ public class MarketResultIntel extends BaseIntelPlugin {
         }
     }
 
-    private String getDistanceToPlayerLY(String format) {
-        float distanceToPlayerLY = Misc.getDistanceToPlayerLY(sectorEntityToken);
-        return String.format(format, distanceToPlayerLY);
+    @Override
+    protected String getTag() {
+        return TAG;
     }
 }
--- src/main/java/stelnet/storage/StorageIntel.java
+++ src/main/java/stelnet/storage/StorageIntel.java
@@ -1,99 +1,64 @@
 package stelnet.storage;
 
 import java.awt.Color;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import com.fs.starfarer.api.campaign.CargoAPI;
-import com.fs.starfarer.api.campaign.FactionAPI;
-import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.econ.SubmarketAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
-import com.fs.starfarer.api.ui.Alignment;
-import com.fs.starfarer.api.ui.SectorMapAPI;
-import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.api.util.Misc;
 
+import stelnet.BaseIntel;
+import stelnet.IntelInfo;
 import stelnet.helper.CargoHelper;
-import stelnet.helper.DistanceHelper;
+import stelnet.ui.Cargo;
+import stelnet.ui.Heading;
+import stelnet.ui.Renderable;
+import stelnet.ui.Ships;
+import stelnet.ui.Size;
+import stelnet.ui.Spacer;
 
-public class StorageIntel extends BaseIntelPlugin {
+public class StorageIntel extends BaseIntel {
 
     public final static String TAG = "stelnetStorage";
 
     private final SubmarketAPI storage;
 
     public StorageIntel(SubmarketAPI storage) {
+        super(storage.getMarket().getFaction(), storage.getMarket().getPrimaryEntity());
         this.storage = storage;
     }
 
     @Override
-    public void createIntelInfo(TooltipMakerAPI info, ListInfoMode mode) {
-        Color bulletColor = getBulletColorForMode(mode);
-        info.addPara(getMarketName(), getTitleColor(mode), 0);
-        info.beginGridFlipped(300f, 1, Misc.getTextColor(), 80f, 10f);
-        info.addToGrid(0, 0, getStorageContent(), "Content", bulletColor);
-        info.addToGrid(0, 1, getFactionName(), "Faction", bulletColor);
-        info.addToGrid(0, 2, getDistance(), "Distance", bulletColor);
-        info.addGrid(3f);
+    public String getIcon() {
+        return getFaction().getCrest();
     }
 
     @Override
-    public void createSmallDescription(TooltipMakerAPI info, float width, float height) {
+    protected IntelInfo getIntelInfo() {
+        return new IntelInfo(getLocationNameWithSystem(), "Content", getStorageContent(), "Faction",
+                getFactionWithRel());
+    }
+
+    @Override
+    protected List<Renderable> getRenderables(Size size) {
         Color baseColor = getFactionForUIColors().getBaseUIColor();
         Color darkColor = getFactionForUIColors().getDarkUIColor();
-        info.addSectionHeading(getMarketName() + " Items", baseColor, darkColor, Alignment.MID, 5f);
-        fakeVerticalSeparator(info, width, 10f);
-        showItems(info);
-        fakeVerticalSeparator(info, width, 20f);
-        info.addSectionHeading(getMarketName() + " Ships", baseColor, darkColor, Alignment.MID, 5f);
-        fakeVerticalSeparator(info, width, 10f);
-        showShips(info);
+        CargoAPI cargo = storage.getCargo();
+        List<FleetMemberAPI> ships = storage.getCargo().getMothballedShips().getMembersListCopy();
+        return Arrays.<Renderable>asList(//
+                new Heading(getLocationName() + " Items", baseColor, darkColor), //
+                new Spacer(10), //
+                new Cargo(cargo, "There are no items in this storage.", size), //
+                new Spacer(10), //
+                new Heading(getLocationName() + " Ships", baseColor, darkColor), //
+                new Spacer(10), //
+                new Ships(ships, "There are no ships in this storage.", size));
     }
 
     @Override
-    public FactionAPI getFactionForUIColors() {
-        return storage.getMarket().getFaction();
-    }
-
-    @Override
-    public String getIcon() {
-        return getFactionForUIColors().getCrest();
-    }
-
-    @Override
-    public Set<String> getIntelTags(SectorMapAPI map) {
-        Set<String> tags = super.getIntelTags(map);
-        tags.add(StorageIntel.TAG);
-        return tags;
-    }
-
-    @Override
-    public SectorEntityToken getMapLocation(SectorMapAPI map) {
-        return storage.getMarket().getPrimaryEntity();
-    }
-
-    @Override
-    public String getSortString() {
-        return String.format("%06.2f", DistanceHelper.getDistanceToPlayerLY(getMapLocation(null)));
-    }
-
-    @Override
-    public boolean isNew() {
-        return false;
-    }
-
-    private String getDistance() {
-        return String.format("%.2fLY", DistanceHelper.getDistanceToPlayerLY(getMapLocation(null)));
-    }
-
-    private String getFactionName() {
-        return getFactionForUIColors().getDisplayName();
-    }
-
-    private String getMarketName() {
-        return storage.getMarket().getName();
+    protected String getTag() {
+        return TAG;
     }
 
     private String getStorageContent() {
@@ -103,20 +68,5 @@ public class StorageIntel extends BaseIntelPlugin {
         String items = itemsCount != 1 ? "s" : "";
         String ships = shipsCount != 1 ? "s" : "";
         return String.format("%d item%s & %d ship%s", itemsCount, items, shipsCount, ships);
-    }
-
-    private void fakeVerticalSeparator(TooltipMakerAPI info, float width, float height) {
-        info.addImage("", width, height, 0);
-    }
-
-    private void showItems(TooltipMakerAPI info) {
-        CargoAPI cargo = storage.getCargo().createCopy();
-        cargo.sort();
-        info.showCargo(cargo, cargo.getStacksCopy().size(), false, 5f);
-    }
-
-    private void showShips(TooltipMakerAPI info) {
-        List<FleetMemberAPI> ships = storage.getCargo().getMothballedShips().getMembersListCopy();
-        info.showShips(ships, ships.size(), false, 5f);
     }
 }
