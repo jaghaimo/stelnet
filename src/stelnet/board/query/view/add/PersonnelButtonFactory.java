5d8cb8fe050b677f62d496c92c19ed6f0a32a79d
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-10-28 13:28:38 +0100

    Add personality

--- src/stelnet/board/query/view/add/PersonnelButtonFactory.java
+++ src/stelnet/board/query/view/add/PersonnelButtonFactory.java
@@ -19,17 +19,13 @@ public class PersonnelButtonFactory implements RenderableFactory {
 
     private final PostTypeButton[] postType;
     private final LevelButton[] level;
+    private final PersonalityButton[] personality;
     private final SkillButton[] skill;
 
     public PersonnelButtonFactory() {
-        postType =
-            new PostTypeButton[] {
-                new PostTypeButton(this, "Administrator", IsPostedAs.admin()),
-                new PostTypeButton(this, "Officer", IsPostedAs.officer()),
-                new PostTypeButton(this, "Mercenary", IsPostedAs.mercenary()),
-                new PostTypeButton(this, "Agent", IsPostedAs.agent()),
-            };
+        postType = getPostTypeButtons();
         level = getLevelButtons();
+        personality = getPersonalityButtons();
         skill = getSkillButtons();
     }
 
@@ -41,6 +37,7 @@ public class PersonnelButtonFactory implements RenderableFactory {
         List<Renderable> containers = new LinkedList<>();
         addPostTypes(containers, textWidth, groupWidth);
         addLevels(containers, textWidth, groupWidth);
+        addPersonalities(containers, textWidth, groupWidth);
         addSkills(containers, textWidth, groupWidth);
         return containers;
     }
@@ -54,16 +51,10 @@ public class PersonnelButtonFactory implements RenderableFactory {
         }
     }
 
-    private void addPostTypes(List<Renderable> containers, float textWidth, float groupWidth) {
-        containers.add(
-            new HorizontalViewContainer(
-                new Paragraph("What type of personnel are you after?", textWidth, 4, Alignment.RMID),
-                new DynamicGroup(groupWidth, postType)
-            )
-        );
-    }
-
     private void addLevels(List<Renderable> containers, float textWidth, float groupWidth) {
+        if (level.length == 0) {
+            return;
+        }
         containers.add(
             new HorizontalViewContainer(
                 new Paragraph("What should be the minimal level?", textWidth, 4, Alignment.RMID),
@@ -72,7 +63,34 @@ public class PersonnelButtonFactory implements RenderableFactory {
         );
     }
 
+    private void addPersonalities(List<Renderable> containers, float textWidth, float groupWidth) {
+        if (personality.length == 0) {
+            return;
+        }
+        containers.add(
+            new HorizontalViewContainer(
+                new Paragraph("What personality should the officer have?", textWidth, 4, Alignment.RMID),
+                new DynamicGroup(groupWidth, personality)
+            )
+        );
+    }
+
+    private void addPostTypes(List<Renderable> containers, float textWidth, float groupWidth) {
+        if (postType.length == 0) {
+            return;
+        }
+        containers.add(
+            new HorizontalViewContainer(
+                new Paragraph("What type of personnel are you after?", textWidth, 4, Alignment.RMID),
+                new DynamicGroup(groupWidth, postType)
+            )
+        );
+    }
+
     private void addSkills(List<Renderable> containers, float textWidth, float groupWidth) {
+        if (skill.length == 0) {
+            return;
+        }
         containers.add(
             new HorizontalViewContainer(
                 new Paragraph("What skills should the officer have?", textWidth, 4, Alignment.RMID),
@@ -86,7 +104,29 @@ public class PersonnelButtonFactory implements RenderableFactory {
         for (int i = 1; i <= Settings.getOfficerMaxLevel(); i++) {
             levelButtons.add(new LevelButton(this, String.valueOf(i), null));
         }
+        if (levelButtons.size() > 0) {
+            levelButtons.get(0).setStateOn(true);
+        }
         return levelButtons.toArray(new LevelButton[] {});
+    }
+
+    private PersonalityButton[] getPersonalityButtons() {
+        return new PersonalityButton[] {
+            new PersonalityButton(this, "officerTimid", null),
+            new PersonalityButton(this, "officerCautious", null),
+            new PersonalityButton(this, "officerSteady", null),
+            new PersonalityButton(this, "officerAggressive", null),
+            new PersonalityButton(this, "officerReckless", null),
+        };
+    }
+
+    private PostTypeButton[] getPostTypeButtons() {
+        return new PostTypeButton[] {
+            new PostTypeButton(this, "typeAdministrator", IsPostedAs.admin()),
+            new PostTypeButton(this, "typeOfficer", IsPostedAs.officer()),
+            new PostTypeButton(this, "typeMercenary", IsPostedAs.mercenary()),
+            new PostTypeButton(this, "typeAgent", IsPostedAs.agent()),
+        };
     }
 
     private SkillButton[] getSkillButtons() {
