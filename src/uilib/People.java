0ac095b0bd04b11a877fcd04333ee6461c539899
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-11-03 11:05:50 +0000

    Move tooltip to separate location

--- src/uilib/People.java
+++ src/uilib/People.java
@@ -1,43 +1,16 @@
 package uilib;
 
 import com.fs.starfarer.api.characters.PersonAPI;
-import com.fs.starfarer.api.ui.PositionAPI;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.api.ui.TooltipMakerAPI.TooltipCreator;
 import com.fs.starfarer.api.ui.TooltipMakerAPI.TooltipLocation;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import uilib.property.Size;
+import uilib.tooltip.PersonTooltip;
 
 public class People extends AbstractRenderable {
 
     private final List<PersonAPI> people;
     private final String emptyDescription;
-
-    @RequiredArgsConstructor
-    public static class PersonTooltip implements TooltipCreator {
-
-        private final PersonAPI person;
-
-        @Override
-        public boolean isTooltipExpandable(Object tooltipParam) {
-            return false;
-        }
-
-        @Override
-        public float getTooltipWidth(Object tooltipParam) {
-            return 200;
-        }
-
-        @Override
-        public void createTooltip(TooltipMakerAPI tooltip, boolean expanded, Object tooltipParam) {
-            tooltip.addImage(person.getPortraitSprite(), 128, 4);
-            PositionAPI imagePosition = tooltip.getPrev().getPosition();
-            imagePosition.inTMid(4);
-            tooltip.addPara(person.getPost(), 4).getPosition().inTL(0, 128 + 8);
-            tooltip.addPara("Level " + String.valueOf(person.getStats().getLevel()), 2);
-        }
-    }
 
     public People(List<PersonAPI> people, String emptyDescription, Size size) {
         this.people = people;
