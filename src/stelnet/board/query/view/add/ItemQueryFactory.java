5e7f5d55b3a9302d0b8522a7d4cb94bd44ab9532
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-11-07 08:41:49 +0000

    Work in progress for item query factory

--- src/stelnet/board/query/view/add/ItemQueryFactory.java
+++ src/stelnet/board/query/view/add/ItemQueryFactory.java
@@ -1,12 +1,26 @@
 package stelnet.board.query.view.add;
 
 import com.fs.starfarer.api.campaign.CargoAPI;
+import java.util.LinkedList;
+import java.util.List;
+import stelnet.CommonL10n;
 import stelnet.board.query.provider.ItemProvider;
 import uilib.Cargo;
+import uilib.Renderable;
 import uilib.RenderableComponent;
+import uilib.VerticalViewContainer;
 import uilib.property.Size;
 
 public class ItemQueryFactory extends PreviewableQueryFactory {
+
+    @Override
+    protected Renderable getContainer() {
+        List<Renderable> elements = new LinkedList<>();
+        beginSection(elements, CommonL10n.WEAPONS);
+        beginSection(elements, CommonL10n.FIGHTER_WINGS);
+        VerticalViewContainer container = new VerticalViewContainer(elements);
+        return container;
+    }
 
     @Override
     protected RenderableComponent getPreviewContent(Size size) {
