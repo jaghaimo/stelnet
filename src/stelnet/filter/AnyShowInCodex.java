e55c1098960e7945ad1727ccbe974c640deb8352
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-11-09 22:36:41 +0000

    Use constants and add no_sell

--- src/stelnet/filter/AnyShowInCodex.java
+++ src/stelnet/filter/AnyShowInCodex.java
@@ -1,11 +1,14 @@
 package stelnet.filter;
 
 import com.fs.starfarer.api.combat.ShipHullSpecAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import com.fs.starfarer.api.loading.FighterWingSpecAPI;
 import com.fs.starfarer.api.loading.HullModSpecAPI;
 import com.fs.starfarer.api.loading.WeaponSpecAPI;
 
 public class AnyShowInCodex extends Filter {
+
+    private static final String HIDE_IN_CODEX = "HIDE_IN_CODEX";
 
     @Override
     public boolean accept(Object object) {
@@ -25,9 +28,9 @@ public class AnyShowInCodex extends Filter {
     }
 
     protected boolean acceptFighterWing(FighterWingSpecAPI fighterWing) {
-        boolean hideInCodex = fighterWing.hasTag("HIDE_IN_CODEX");
-        boolean noSell = fighterWing.hasTag("no_sell");
-        return !hideInCodex && !noSell;
+        boolean hideInCodex = fighterWing.hasTag(HIDE_IN_CODEX);
+        boolean isNoSell = fighterWing.hasTag(Tags.NO_SELL);
+        return !hideInCodex && !isNoSell;
     }
 
     protected boolean acceptHullMod(HullModSpecAPI hullMod) {
@@ -39,9 +42,9 @@ public class AnyShowInCodex extends Filter {
     }
 
     protected boolean acceptWeapon(WeaponSpecAPI weapon) {
-        boolean isNoDrop = weapon.hasTag("no_drop");
-        boolean isOmega = weapon.hasTag("omega");
-        boolean isRestricted = weapon.hasTag("restricted");
-        return !isNoDrop && !isOmega && !isRestricted;
+        boolean isNoDrop = weapon.hasTag(Tags.NO_DROP);
+        boolean isNoSell = weapon.hasTag(Tags.NO_SELL);
+        boolean isRestricted = weapon.hasTag(Tags.RESTRICTED);
+        return !isNoDrop && !isNoSell && !isRestricted;
     }
 }
