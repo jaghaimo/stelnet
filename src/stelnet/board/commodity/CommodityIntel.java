e4179682021d6135503efe52638cf6351b93a260
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-11-16 21:35:37 +0000

    Remove this enum from translations

--- src/stelnet/board/commodity/CommodityIntel.java
+++ src/stelnet/board/commodity/CommodityIntel.java
@@ -113,7 +113,7 @@ public class CommodityIntel extends BaseIntel {
     private void addRelationship(List<Renderable> renderables, float width) {
         FactionAPI faction = marketWrapper.getFaction();
         RelationshipAPI relationship = faction.getRelToPlayer();
-        String translatedRep = L10n.get(relationship.getLevel());
+        String translatedRep = relationship.getLevel().getDisplayName();
         Paragraph relationshipRenderable = new Paragraph(
             L10n.get(CommonL10n.INTEL_OWNER_RELATIONSHIP, translatedRep),
             width
