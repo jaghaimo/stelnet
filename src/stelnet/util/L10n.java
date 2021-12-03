fb7f9248a118105c918f253efe9f2cba1f6e3b4c
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-12-02 22:48:12 -0800

    split translatins

--- src/stelnet/util/L10n.java
+++ src/stelnet/util/L10n.java
@@ -1,6 +1,8 @@
 package stelnet.util;
 
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.log4j.Log4j;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,37 +10,38 @@ import org.json.JSONObject;
 @Log4j
 public class L10n {
 
-    private static transient JSONObject translations;
+    private static transient Map<String, JSONObject> translations = new HashMap<>();
 
-    public static void init() {
-        if (translations == null) {
-            translations = SettingsUtils.loadRawJson("translation/all.json");
+    public static void init(String filename) {
+        if (!translations.containsKey(filename)) {
+            String path = "translation/" + filename;
+            translations.put(filename, SettingsUtils.loadRawJson(path));
         }
     }
 
     public static String get(Enum<?> enumKey, Object... args) {
-        init();
-        String key = enumToKey(enumKey);
-        return get(key, args);
+        String filename = enumToFilename(enumKey);
+        init(filename);
+        return get(filename, enumKey.name(), args);
     }
 
-    private static String get(String key, Object... args) {
+    private static String get(String filename, String key, Object... args) {
         try {
-            String translation = translations.getString(key);
+            String translation = translations.get(filename).getString(key);
             return MessageFormat.format(translation, args);
         } catch (JSONException e) {
-            log.warn("Missing translation for key " + key);
+            log.warn("Missing translation for key " + key + " in " + filename);
             return key;
         }
     }
 
-    private static String enumToKey(Enum<?> enumKey) {
-        String enumPrefix = enumKey.getClass().getName().toUpperCase();
-        enumPrefix = enumPrefix.replaceAll("L10N", "");
-        enumPrefix = enumPrefix.replaceAll("\\.", "\\_");
-        enumPrefix = removeTail(enumPrefix);
-        enumPrefix = removeHead(enumPrefix);
-        return enumPrefix + "_" + enumKey.name();
+    private static String enumToFilename(Enum<?> enumKey) {
+        String enumName = enumKey.getClass().getName().toLowerCase();
+        enumName = enumName.replaceAll("l10n", "");
+        enumName = enumName.replaceAll("\\.", "\\_");
+        enumName = removeTail(enumName);
+        enumName = removeHead(enumName);
+        return enumName + ".json";
     }
 
     private static String removeTail(String string) {
