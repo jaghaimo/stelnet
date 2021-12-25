45b85ecc7828e29dc43fd3c6a8786b27986abaa9
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-12-25 19:54:11 +0000

    compose not known modspec

--- src/stelnet/filter/CargoStackNotKnownModspec.java
+++ src/stelnet/filter/CargoStackNotKnownModspec.java
@@ -2,14 +2,17 @@ package stelnet.filter;
 
 import com.fs.starfarer.api.campaign.CargoStackAPI;
 import lombok.EqualsAndHashCode;
+import stelnet.filter.CargoStackIsType.Type;
 import stelnet.util.SectorUtils;
 
 @EqualsAndHashCode(callSuper = false)
 public final class CargoStackNotKnownModspec extends CargoStackFilter {
 
+    private final Filter isCargoStack = new CargoStackIsType(Type.MODSPEC);
+
     @Override
     protected boolean acceptCargoStack(CargoStackAPI cargoStack) {
-        if (!cargoStack.isModSpecStack()) {
+        if (!isCargoStack.accept(cargoStack)) {
             return true;
         }
         String hullModId = cargoStack.getHullModSpecIfHullMod().getId();
