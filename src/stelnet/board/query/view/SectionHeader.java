3fbea083d6f50a8969a4be5ec87f714d559be0bf
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-02-10 12:44:08 +0000

    extends section header functionality

--- src/stelnet/board/query/view/SectionHeader.java
+++ src/stelnet/board/query/view/SectionHeader.java
@@ -12,16 +12,27 @@ import uilib.property.Size;
 @RequiredArgsConstructor
 public class SectionHeader extends HeadingWithButtons {
 
-    private final FilteringButton[] buttons;
+    private final FilteringButton[] filterButtons;
+    private final FilteringButton filterLogicButton;
     private final String heading;
     private final boolean isEnabled;
 
     public SectionHeader(float width, Enum<?> label, boolean isEnabled) {
-        this(width, label, isEnabled, null);
+        this(width, label, isEnabled, null, null);
     }
 
-    public SectionHeader(float width, Enum<?> label, boolean isEnabled, FilteringButton[] buttons) {
-        this(buttons, L10n.get(label), isEnabled);
+    public SectionHeader(float width, Enum<?> label, boolean isEnabled, FilteringButton[] filterButtons) {
+        this(width, label, isEnabled, filterButtons, null);
+    }
+
+    public SectionHeader(
+        float width,
+        Enum<?> label,
+        boolean isEnabled,
+        FilteringButton[] filterButtons,
+        FilteringButton filterLogicButton
+    ) {
+        this(filterButtons, filterLogicButton, L10n.get(label), isEnabled);
         setSize(new Size(width, UiConstants.DEFAULT_ROW_HEIGHT + UiConstants.DEFAULT_SPACER * 3));
         setWithScroller(false);
     }
@@ -31,14 +42,26 @@ public class SectionHeader extends HeadingWithButtons {
         tooltip.addSpacer(UiConstants.DEFAULT_SPACER);
         tooltip.addSpacer(UiConstants.DEFAULT_SPACER);
         renderQueryHeading(tooltip, isEnabled, heading);
-        if (buttons != null) {
-            UIComponentAPI first = renderFirstButton(
-                new SelectDeselectButton(CommonL10n.NONE, isEnabled, false, buttons),
-                getSize().getWidth(),
-                tooltip
-            );
-            renderNextButton(new SelectDeselectButton(CommonL10n.ALL, isEnabled, true, buttons), tooltip, first);
+        if (filterButtons != null) {
+            renderButtons(tooltip);
         }
         tooltip.addSpacer(UiConstants.DEFAULT_SPACER);
+    }
+
+    private void renderButtons(TooltipMakerAPI tooltip) {
+        UIComponentAPI currentComponent = renderFirstButton(
+            new SelectDeselectButton(CommonL10n.NONE, isEnabled, false, filterButtons),
+            getSize().getWidth(),
+            tooltip
+        );
+        currentComponent =
+            renderNextButton(
+                new SelectDeselectButton(CommonL10n.ALL, isEnabled, true, filterButtons),
+                tooltip,
+                currentComponent
+            );
+        if (filterLogicButton != null) {
+            renderNextButton(filterLogicButton, tooltip, currentComponent);
+        }
     }
 }
