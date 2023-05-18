10f9770385f0cda2d0f343a4e747fdbd2bd14ed2
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2023-05-18 16:28:50 +0100

    Add logging when skipping a callback

--- src/uilib2/intel/CallbackAwareIntel.java
+++ src/uilib2/intel/CallbackAwareIntel.java
@@ -3,21 +3,27 @@ package uilib2.intel;
 import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
 import com.fs.starfarer.api.ui.IntelUIAPI;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public abstract class CallbackAwareIntel extends BaseIntelPlugin {
 
     public void buttonPressConfirmed(Object buttonId, IntelUIAPI ui) {
         try {
             IntelUiCallback callback = (IntelUiCallback) buttonId;
             callback.onConfirm(ui);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            log.debug("Skipping unsupported 'buttonPressConfirmed' buttonId type'");
+        }
     }
 
     public void buttonPressCancelled(Object buttonId, IntelUIAPI ui) {
         try {
             IntelUiCallback callback = (IntelUiCallback) buttonId;
             callback.onCancel(ui);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            log.debug("Skipping unsupported 'buttonPressCancelled' buttonId type'");
+        }
     }
 
     @Override
@@ -25,7 +31,9 @@ public abstract class CallbackAwareIntel extends BaseIntelPlugin {
         try {
             IntelUiCallback callback = (IntelUiCallback) buttonId;
             callback.getPrompt().show(tooltip);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            log.debug("Skipping unsupported 'createConfirmationPrompt' buttonId type'");
+        }
     }
 
     @Override
