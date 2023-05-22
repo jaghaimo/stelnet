82858c3537cac1c2e4eaab09b413f887d0ef2101
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2023-05-22 18:23:06 +0100

    Implement CaptainsLog intel filtering

--- src/stelnet/board/exploration/factory/FilterFactory.java
+++ src/stelnet/board/exploration/factory/FilterFactory.java
@@ -18,7 +18,6 @@ import stelnet.filter.IntelContainsTitle;
 import stelnet.filter.IntelIsClass;
 import stelnet.filter.IntelLocationHasMemory;
 import stelnet.filter.LogicalAnd;
-import stelnet.filter.LogicalFalse;
 import stelnet.filter.LogicalNot;
 import stelnet.filter.LogicalOr;
 
@@ -35,12 +34,12 @@ public class FilterFactory {
     private void addTypes() {
         Map<ExplorationL10n, Filter> localMap = new LinkedHashMap<>();
         localMap.put(ExplorationL10n.TYPE_ANALYZE_MISSION, new IntelIsClass(AnalyzeEntityMissionIntel.class));
-        localMap.put(ExplorationL10n.TYPE_ANY_RUINS, getCaptainsLogFilter(new LogicalFalse()));
-        localMap.put(ExplorationL10n.TYPE_COMM_RELAY, getCaptainsLogFilter(new LogicalFalse()));
+        localMap.put(ExplorationL10n.TYPE_ANY_RUINS, getCaptainsLogFilter(new IntelContainsTitle("Ruins")));
+        localMap.put(ExplorationL10n.TYPE_COMM_RELAY, getCaptainsLogFilter(new IntelContainsTitle("Comm Relay")));
         localMap.put(ExplorationL10n.TYPE_HISTORIAN_OFFER, new IntelIsClass(BaseHistorianOffer.class));
         localMap.put(ExplorationL10n.TYPE_MEMORY_BANK, new IntelIsClass(BreadcrumbIntel.class));
         localMap.put(ExplorationL10n.TYPE_RAIDING_BASE, getRaidingBaseFilter());
-        localMap.put(ExplorationL10n.TYPE_SALVAGEABLE, getCaptainsLogFilter(new LogicalFalse()));
+        localMap.put(ExplorationL10n.TYPE_SALVAGEABLE, getCaptainsLogFilter(new IntelContainsTitle("Salvageable")));
         localMap.put(ExplorationL10n.TYPE_STORY_MISSION, getStoryMissionFilter());
         localMap.put(ExplorationL10n.TYPE_SURVEY_MISSION, new IntelIsClass(SurveyPlanetMissionIntel.class));
         Filter otherFilter = getOtherFilter(localMap);
@@ -73,7 +72,7 @@ public class FilterFactory {
     private Filter getCaptainsLogFilter(Filter actualFilter) {
         return new LogicalAnd(
             Arrays.<Filter>asList(new IntelLocationHasMemory("$captainsLogIntel"), actualFilter),
-            "CaptainsLog"
+            "CaptainsLog Custom Filter"
         );
     }
 
