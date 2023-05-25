549c3b5934fab1ff5234d32bc220f87e4673f567
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2023-05-25 20:18:10 +0100

    Refactor show skills / tooltip

--- src/uilib/ShowSkills.java
+++ src/uilib/ShowSkills.java
@@ -1,51 +1,20 @@
 package uilib;
 
 import com.fs.starfarer.api.characters.MutableCharacterStatsAPI.SkillLevelAPI;
-import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
-import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import stelnet.filter.Filter;
-import stelnet.filter.LogicalOr;
-import stelnet.filter.SkillIsAdmin;
-import stelnet.filter.SkillIsCombatOfficer;
-import stelnet.util.CollectionUtils;
 
 @RequiredArgsConstructor
 public class ShowSkills extends RenderableComponent {
 
-    private final PersonAPI person;
+    private final List<SkillLevelAPI> skills;
+    private final String title;
 
     @Override
     public void render(TooltipMakerAPI tooltip) {
-        // TODO: Move addPara away and make strings localizable
-        List<SkillLevelAPI> skills = person.getStats().getSkillsCopy();
-        CollectionUtils.reduce(
-            skills,
-            new LogicalOr(Arrays.<Filter>asList(new SkillIsAdmin(), new SkillIsCombatOfficer()), "Any Skill")
-        );
-        String level;
-        if (skills.isEmpty()) {
-            level =
-                String.format(
-                    "Level %d %s with no notable skills.",
-                    person.getStats().getLevel(),
-                    person.getPost().toLowerCase()
-                );
-            tooltip.addPara(level, UiConstants.DEFAULT_BUTTON_PADDING);
-            tooltip.addSpacer(UiConstants.DEFAULT_BUTTON_PADDING);
-            return;
-        }
-
-        level =
-            String.format(
-                "Level %d %s with the following skills:",
-                person.getStats().getLevel(),
-                person.getPost().toLowerCase()
-            );
-        tooltip.addPara(level, UiConstants.DEFAULT_BUTTON_PADDING);
+        tooltip.addPara(title, UiConstants.DEFAULT_BUTTON_PADDING);
         tooltip.addSpacer(UiConstants.DEFAULT_BUTTON_PADDING);
         for (SkillLevelAPI skill : skills) {
             showSkill(tooltip, skill);
