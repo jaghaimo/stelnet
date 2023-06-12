adec5b74b2450dfe7b4c4f9da8660066bc011f12
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2023-06-12 10:51:39 +0100

    Use new LargeIntel class

--- src/stelnet/board/commodity2/NewCommodityBoard.java
+++ src/stelnet/board/commodity2/NewCommodityBoard.java
@@ -1,23 +1,22 @@
 package stelnet.board.commodity2;
 
-import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
+import java.util.Collections;
 import lombok.Getter;
 import stelnet.util.ModConstants;
 import stelnet.util.StelnetHelper;
+import uilib2.Drawable;
+import uilib2.Layout;
+import uilib2.intel.LargeIntel;
+import uilib2.layouts.BasicLayout;
 
 @Getter
-public class NewCommodityBoard extends BaseIntelPlugin {
+public class NewCommodityBoard extends LargeIntel {
 
     private final String icon = StelnetHelper.getSpriteName("commodity");
     private final String intelTags = ModConstants.TAG_COMMODITY;
 
     @Override
-    public boolean hasSmallDescription() {
-        return false;
-    }
-
-    @Override
-    public boolean hasLargeDescription() {
-        return true;
+    protected Layout getLayout() {
+        return new BasicLayout(Collections.<Drawable>emptyList());
     }
 }
