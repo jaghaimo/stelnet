4e4b9fbb5eb0c153ad772b5e8eeb187336704d05
Volt <@>
2025-05-02 12:05:19 +0200

    Change Commodities intel tab to respect markets excluder
    * Address IDE's code quality warnings.

--- src/stelnet/board/commodity/market/MarketFactory.java
+++ src/stelnet/board/commodity/market/MarketFactory.java
@@ -2,13 +2,14 @@ package stelnet.board.commodity.market;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import stelnet.board.commodity.price.Price;
 import stelnet.filter.MarketNotHidden;
 import stelnet.util.CollectionUtils;
+import stelnet.util.Excluder;
+
 
 @RequiredArgsConstructor
 public abstract class MarketFactory {
@@ -17,24 +18,18 @@ public abstract class MarketFactory {
 
     public List<MarketAPI> createMarkets() {
         List<MarketAPI> markets = Global.getSector().getEconomy().getMarketsCopy();
-        CollectionUtils.reduce(markets, new MarketNotHidden());
+        CollectionUtils.reduce(markets, Arrays.asList(new MarketNotHidden(), Excluder.getMarketFilter()));
         filterMarkets(markets);
         sortMarkets(markets);
         return markets;
     }
 
     protected void sortMarkets(List<MarketAPI> markets) {
-        Collections.sort(
-            markets,
-            new Comparator<MarketAPI>() {
-                @Override
-                public int compare(MarketAPI marketA, MarketAPI marketB) {
-                    float priceA = getPrice().getUnitPrice(marketA);
-                    float priceB = getPrice().getUnitPrice(marketB);
-                    return (int) Math.signum(priceB - priceA);
-                }
-            }
-        );
+        markets.sort((marketA, marketB) -> {
+            float priceA = getPrice().getUnitPrice(marketA);
+            float priceB = getPrice().getUnitPrice(marketB);
+            return (int) Math.signum(priceB - priceA);
+        });
     }
 
     protected abstract Price getPrice();
